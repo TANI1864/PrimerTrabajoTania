07f265875a3d53d221a448ebc0c20fd6a9e637f4
TANI1864 <taniabeltran2020@gmail.com>
2025-02-21 01:05:29 -0500

    Se agrego algo nuevo

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Que dificil");
-        System.out.println("otros cambios");
+        System.out.println("Ya son las doce de la noche");
+        System.out.println("Mas cambios");
     }
 }
