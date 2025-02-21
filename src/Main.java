1ec3a7b6c2392fa65c3c247d71598ecadd5116c3
TANI1864 <taniabeltran2020@gmail.com>
2025-02-21 09:12:54 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Que dificil");
+        System.out.println("Me gusta mucho el sushi");
         System.out.println("otros cambios");
     }
 }
