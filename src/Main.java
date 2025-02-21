91db2540506fca329ef8da69495b0b057584b050
TANI1864 <taniabeltran2020@gmail.com>
2025-02-21 09:18:06 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ya son las doce de la noche");
+        System.out.println("Buenos dias");
         System.out.println("Mas cambios");
     }
 }
