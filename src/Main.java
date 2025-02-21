c5056916526097cba8f992425ea1d21342064189
TANI1864 <taniabeltran2020@gmail.com>
2025-02-21 00:27:10 -0500

    Cambiar cambios

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola soy Tania Lorena Beltran Lara y este es mi primer trabajo");
-        System.out.println("Se estan haciendo cambios");
+        System.out.println("Que dificil");
+        System.out.println("otros cambios");
     }
 }
